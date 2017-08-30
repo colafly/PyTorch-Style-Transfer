94a69ec564f6fa7fb3eecdd761f7d88203e4cfda
colafly <aauddrey@gmail.com>
2017-08-29 23:50:11 -0700

    Update option.py

--- experiments/option.py
+++ experiments/option.py
@@ -9,40 +9,40 @@ class Options():
 		# training args
 		# train_arg = subparsers.add_parser("train",
 		# 							help="parser for training arguments")
-		train_arg.add_argument("--net-type", type=str, default="v2",
+		parser.add_argument("--net-type", type=str, default="v2",
 								help="type of the network, default is v2")
-		train_arg.add_argument("--ngf", type=int, default=128,
+		parser.add_argument("--ngf", type=int, default=128,
 								help="number of generator filter channels, default 128")
-		train_arg.add_argument("--epochs", type=int, default=2,
+		parser.add_argument("--epochs", type=int, default=2,
 								help="number of training epochs, default is 2")
-		train_arg.add_argument("--batch-size", type=int, default=4,
+		parser.add_argument("--batch-size", type=int, default=4,
 								help="batch size for training, default is 4")
-		train_arg.add_argument("--dataset", type=str, default="dataset/",
+		parser.add_argument("--dataset", type=str, default="dataset/",
 								help="path to training dataset, the path should point to a folder "
 								"containing another folder with all the training images")
-		train_arg.add_argument("--style-folder", type=str, default="images/9styles/",
+		parser.add_argument("--style-folder", type=str, default="images/9styles/",
 								help="path to style-folder")
-		train_arg.add_argument("--vgg-model-dir", type=str, default="models/",
+		parser.add_argument("--vgg-model-dir", type=str, default="models/",
 								help="directory for vgg, if model is not present in the directory it is downloaded")
-		train_arg.add_argument("--save-model-dir", type=str, default="models/",
+		parser.add_argument("--save-model-dir", type=str, default="models/",
 								help="path to folder where trained model will be saved.")
-		train_arg.add_argument("--image-size", type=int, default=256,
+		parser.add_argument("--image-size", type=int, default=256,
 								help="size of training images, default is 256 X 256")
-		train_arg.add_argument("--style-size", type=int, default=512,
+		parser.add_argument("--style-size", type=int, default=512,
 								help="size of style-image, default is the original size of style image")
-		train_arg.add_argument("--cuda", type=int, default=1,
+		parser.add_argument("--cuda", type=int, default=1,
 								help="set it to 1 for running on GPU, 0 for CPU")
-		train_arg.add_argument("--seed", type=int, default=42,
+		parser.add_argument("--seed", type=int, default=42,
 								help="random seed for training")
-		train_arg.add_argument("--content-weight", type=float, default=1.0,
+		parser.add_argument("--content-weight", type=float, default=1.0,
 								help="weight for content-loss, default is 1.0")
-		train_arg.add_argument("--style-weight", type=float, default=5.0,
+		parser.add_argument("--style-weight", type=float, default=5.0,
 								help="weight for style-loss, default is 5.0")
-		train_arg.add_argument("--lr", type=float, default=1e-3,
+		parser.add_argument("--lr", type=float, default=1e-3,
 								help="learning rate, default is 0.001")
-		train_arg.add_argument("--log-interval", type=int, default=500,
+		parser.add_argument("--log-interval", type=int, default=500,
 								help="number of images after which the training loss is logged, default is 500")
-		train_arg.add_argument("--resume", type=str, default=None,
+		parser.add_argument("--resume", type=str, default=None,
 								help="resume if needed")
 
 # 		# optim args (Gatys CVPR 2016)
